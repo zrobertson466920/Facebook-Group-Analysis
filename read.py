862cdc59132730fb5378da7f031a861c90f398f3
Zachary <zachary_w_robertson@hotmail.com>
2018-08-12 16:27:26 -0500

    Added basic reaction analysis

--- read.py
+++ read.py
@@ -8,6 +8,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
 
+import datetime
+
 from collections import Counter
 
 from wordcloud import WordCloud
@@ -65,14 +67,13 @@ def common_words(text,num,pl = -1):
 
     return freq
 
-# Constructs a frequnecy list based off of raw text (no matrix)
-def tag_freq(text,num,pl = -1):
-    word_tokens = word_tokenize(fix_text(text).lower())
-    tag_indices = [i for i, tag in enumerate(word_tokens) if tag == '@']
-    tags = []
-    for i in tag_indices:
-        tags.append(word_tokens[i]+word_tokens[i+1])
-    freq = Counter(tags)
+# Constructs a frequnecy list based off of tag_matrix
+def tag_freq(freq_tuples,num,pl = -1):
+
+    freq = []
+    for (tagger,tagged),count in freq_tuples:
+        freq += [tagged] * count
+    freq = Counter(freq)
 
     if pl == -1:
         return freq
@@ -88,8 +89,30 @@ def tag_freq(text,num,pl = -1):
         plt.xticks(rotation=70)
         plt.show()
 
+def reac_freq(reac_tuples,num,pl = -1):
+
+    freq = []
+    for (tagger,reaction,tagged),count in reac_tuples:
+        freq += [tagged] * count
+    freq = Counter(freq)
+
+    if pl == -1:
+        return freq
+    elif pl == 0:
+        sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
+        for key,val in sorted_by_second[0:num]:
+            print(str(key) + ":" + str(val))
+    else:
+        sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
+        freq = sorted_by_second[:num]
+        plt.bar(range(len(freq)), [val[1] for val in freq], align='center')
+        plt.xticks(range(len(freq)), [val[0] for val in freq])
+        plt.xticks(rotation=70)
+        plt.show()
+
+
 # Constructs a dictionary of edge frequencies where edges are (sender,tagged)
-def tag_matrix(timeline,num):
+def tag_matrix(timeline,num = None):
     t_dict = {}
     tags = []
     for message in timeline:
@@ -104,7 +127,39 @@ def tag_matrix(timeline,num):
     sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
     freq = sorted_by_second[:num]
     print(freq)
+    return freq
 
+# Constructs a dictionary of reaction frequencies where edges (reaction wheighted) are (sender,reaction,receiver)
+def reac_matrix(timeline,num = None):
+    reactions = []
+    for message in timeline:
+        try:
+            for reac in message['reactions']:
+                reactions.append((reac['actor'],fix_text(reac['reaction']),message['sender_name']))
+        except:
+            continue
+    freq = Counter(reactions)
+    sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
+    freq = sorted_by_second[:num]
+    print(freq)
+    return freq
+
+# Plots the number of messages sent in each hour slot during the day
+def activity_plot(timestamps):
+    times = []
+    for stamp in timestamps:
+        temp = datetime.datetime.fromtimestamp(stamp)
+        times.append((temp.hour+temp.minute / 60 - 1) % 24)
+    plt.hist(times,24)
+    plt.show()
+
+# Creates a list of time gaps between a person's message to the group and the first reply back
+def reply_gap(timeline,person):
+    reply_gap = []
+    for i in range(len(timeline)-1):
+        if timeline[i]['sender_name'] == person and timeline[i+1]['sender_name'] != person:
+            reply_gap.append(timeline[i+1]['timestamp']-timeline[i]['timestamp'])
+    return reply_gap
 
 def main():
 
@@ -133,7 +188,15 @@ def main():
 
     #Histograms
     #freq_words = common_words(text,15,1)
-    #tag_freq(text,10,1)
+    #tag_freq(tag_matrix(timelines['Zach Robertson']),10,1)
     #tag_matrix(timeline,10)
+    #reac_freq(reac_matrix(timeline),10,1)
+    #reac_matrix(timeline,10)
+    #activity_plot(timestamps)
+
+    time_gaps = reply_gap(timeline,'Dave Marques')
+    time_gaps = [tg for tg in time_gaps if tg < 100]
+    plt.hist(time_gaps,100)
+    plt.show()
 
 main()
