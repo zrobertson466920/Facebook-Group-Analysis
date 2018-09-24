99081973471930c79f24e0e931fe00ad6c743d29
zrobertson <zachary_w_robertson@hotmail.com>
2018-09-24 09:36:18 -0500

    Cleaned Up Code and Example

--- fgroupanalysis.py
+++ fgroupanalysis.py
@@ -22,12 +22,19 @@ from collections import Counter
 from wordcloud import WordCloud
 
 class Timeline:
-    def __init__(self,raw = None):
-        if raw == None:
+    def __init__(self,data = None,raw = None):
+        if data != None:
+            timeline = data['messages']
+            timeline.reverse()
+            self.messages = timeline
+            return
+        elif raw == None:
             self.messages = []
             return
         else:
-            data = json.loads(raw)
+            with open(raw) as f:
+                data = json.load(f)
+            f.close()
             timeline = data['messages']
             timeline.reverse()
             self.messages = timeline
@@ -46,6 +53,16 @@ class Timeline:
                 timelines[message['sender_name']].messages.append(message)
         return timelines
 
+    def filter_by_word(self,word):
+        word_timeline = Timeline()
+        for message in self.messages:
+            try:
+                if message['content'].find(word) != -1:
+                    word_timeline.messages.append(message)
+            except:
+                pass
+        return word_timeline
+
     # Fixes encoding errors
     def to_text(self,file):
         f = open(file,'w')
@@ -56,8 +73,19 @@ class Timeline:
                 continue
         f.close()
 
+    # Most Reacted Messages
+    def message_reacs(self):
+        mr_count = []
+        for message in self.messages:
+            try:
+                mr_count.append((message['sender_name'],fix_text(message['content']),len(message['reactions'])))
+            except:
+                pass
+        sorted_by_second = sorted(mr_count, key=lambda tup: tup[2], reverse = True)
+        return sorted_by_second
+
     # Constructs a dictionary of edge frequencies where edges are (sender,tagged)
-    def tag_matrix(self,num = None):
+    def tag_matrix(self,tagger = True, tagged = True):
         t_dict = {}
         tags = []
         for message in self.messages:
@@ -65,28 +93,59 @@ class Timeline:
                 word_tokens = word_tokenize(fix_text(message['content']).lower())
                 tag_indices = [i for i, tag in enumerate(word_tokens) if tag == '@']
                 for i in tag_indices:
-                    tags.append((message['sender_name'],word_tokens[i] + word_tokens[i+1]))
+                    if tagger == True:
+                        a = word_tokenize(message['sender_name'])[0].lower()
+                    else:
+                        a = None
+                    if tagged == True:
+                        b = word_tokens[tag_indices[0]+1]
+                    else:
+                        b = None
+                    tags.append([a,b])
             except:
                 continue
-        freq = Counter(tags)
+
+        new_tags = []
+        for item in tags:
+            item = [i for i in item if i != None]
+            new_tags.append(tuple(item))
+
+        freq = Counter(new_tags)
         sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
-        freq = sorted_by_second[:num]
-        print(freq)
+        freq = sorted_by_second
         return freq
 
     # Constructs a dictionary of reaction frequencies where edges (reaction wheighted) are (sender,reaction,receiver)
-    def reac_matrix(self,num = None):
+    def reac_matrix(self,num = None,sender = True, reaction = True, receiver = True):
         reactions = []
         for message in self.messages:
             try:
                 for reac in message['reactions']:
-                    reactions.append((reac['actor'],fix_text(reac['reaction']),message['sender_name']))
+                    if sender == True:
+                        a = reac['actor']
+                    else:
+                        a = None
+                    if reaction == True:
+                        b = fix_text(reac['reaction'])
+                    else:
+                        b = None
+                    if receiver == True:
+                        c = message['sender_name']
+                    else:
+                        c = None
+                    reactions.append([a,b,c])
             except:
                 continue
-        freq = Counter(reactions)
+
+        new_reactions = []
+        for item in reactions:
+            item = [i for i in item if i != None]
+            new_reactions.append(tuple(item))
+
+        freq = Counter(new_reactions)
         sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
         freq = sorted_by_second[:num]
-        print(freq)
+
         return freq
 
     # Creates a list of time gaps between a person's message to the group and the first reply back
@@ -94,28 +153,39 @@ class Timeline:
         reply_gap = []
         for i in range(len(self.messages)-1):
             if self.messages[i]['sender_name'] == person and self.messages[i+1]['sender_name'] != person:
-                reply_gap.append(self.messages[i+1]['timestamp']-self.messages[i]['timestamp'])
+                reply_gap.append(self.messages[i+1]['timestamp_ms']-self.messages[i]['timestamp_ms'])
         return reply_gap
 
     def get_timestamps(self):
         timestamps = []
         for message in self.messages:
-            timestamps.append(message['timestamp'])
+            timestamps.append(message['timestamp_ms'] / 1000)
         return timestamps
 
     def raw_text(self):
-        text = []
+        text = ''
         for message in self.messages:
             try:
-                text += "\n" + message['content']
+                if message['content'].find('sent a photo') == -1:
+                    text += "\n" + message['content']
             except:
                 continue
         return text
 
-
+# Combine Messages in seperate timelines
+def combine(timelines):
+    new_timeline = {}
+    new_messages = []
+    for timeline in timelines:
+        for message in timeline.messages:
+            new_messages.append((message,message['timestamp_ms']))
+    new_messages = sorted(new_messages, key=lambda tup: tup[1],reverse = True)
+    new_timeline['messages'] , timestamps = zip(*new_messages)
+    new_timeline['messages'] = list(new_timeline['messages'])
+    return Timeline(data = new_timeline)
 
 # Will return the top 'num' words as a list of tuples [(word,num)]
-def common_words(text,num,pl = -1):
+def common_words(text,pl = False):
     #From NTLK library
     stop_words = set(stopwords.words("english"))
 
@@ -128,12 +198,8 @@ def common_words(text,num,pl = -1):
     freq = Counter(filtered_words)
 
     # Show a plot/list or nothing depending on pl
-    if pl == -1:
+    if pl == False:
         return freq
-    elif pl == 0:
-        sorted_by_second = sorted(freq.items(), key=lambda tup: tup[1], reverse = True)
-        for key,val in sorted_by_second[0:num]:
-            print(str(key) + ":" + str(val))
     else:
         wordcloud = WordCloud(width=1600, height=800).generate_from_frequencies(freq)
         plt.figure(figsize = (20,10), facecolor = 'k')
@@ -190,16 +256,20 @@ def reac_freq(reac_tuples,num,pl = -1):
 # Plots the number of messages sent in each hour slot during the day
 # time_difference (hours) will account for timezone differences
 # aka time_difference = 1 assumes you're one hour ahead
-def activity_plot(timestamps,time_difference = 0):
+def activity_plot(timestamps,time_difference = 0,daily = True):
     times = []
-    for stamp in timestamps:
-        temp = datetime.datetime.fromtimestamp(stamp)
-        times.append((temp.hour+temp.minute / 60 - time_difference) % 24)
+    if daily == True:
+        for stamp in timestamps:
+            temp = datetime.datetime.fromtimestamp(stamp)
+            times.append((temp.hour+temp.minute / 60 - time_difference) % 24)
+    else:
+        for stamp in timestamps:
+            times.append(stamp)
     return times
 
 # Groups Timestamps into Clusters
 # (5-10) ~ are good numbers for min_cluster
-def cluster(timestamps,min_cluster):
+def cluster(timestamps,min_cluster = 7,plot = False):
 
     X = np.array(timestamps)
     X = X.reshape(-1,1)
@@ -218,17 +288,18 @@ def cluster(timestamps,min_cluster):
     cmap = plt.cm.get_cmap("Spectral")
     colors = [cmap(np.random.rand(1)[0]) for each in np.linspace(0, 1, len(unique_lables))]
 
-    for k, col in zip(unique_lables, colors):
-        if k == -1:
-            # Black used for noise
-            col = [0, 0, 0, 1]
-            continue
+    if plot == True:
+        for k, col in zip(unique_lables, colors):
+            if k == -1:
+                # Black used for noise
+                col = [0, 0, 0, 1]
+                continue
 
-        class_member_mask = (labels == k)
-        xy = X[class_member_mask]
-        plt.plot(xy[:], [0] * len(xy), 'o', c=tuple(col), linewidth = 7)
+            class_member_mask = (labels == k)
+            xy = X[class_member_mask]
+            plt.plot(xy[:], [0] * len(xy), 'o', c=tuple(col), linewidth = 7)
 
-    plt.title("Estimated Number of Clusters: %d" % n_clusters_)
-    plt.show()
+        plt.title("Estimated Number of Clusters: %d" % n_clusters_)
+        plt.show()
 
     return labels
