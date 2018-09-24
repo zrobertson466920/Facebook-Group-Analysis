d22620ef449b6005af23dc27a5d7f04e025340d0
zrobertson <zachary_w_robertson@hotmail.com>
2018-09-24 14:20:52 -0500

    Method documentation

--- fgroupanalysis.py
+++ fgroupanalysis.py
@@ -38,13 +38,12 @@ class Timeline:
             self.messages = timeline
             return
 
-    # Splits the messages by sender
     def partition(self):
         """
         Splits the messages by participant in the group chat
 
         Returns:
-            dictionary: A dictionary of timelines sorted by participant name.
+            timelines: A dictionary of timelines sorted by participant name.
         """
         actors = []
         timelines = {}
@@ -58,6 +57,19 @@ class Timeline:
         return timelines
 
     def filter_by_word(self,word):
+        """
+        Will filter out all messages from the timeline that don't contain a specific string
+
+        Args:
+            word: A string to filter by
+
+        Returns:
+            word_timeline: The filtered timeline
+
+        Todo:
+            Generalize to the [word] case. Also rename to 'filter_by_phrase'
+
+        """
         word_timeline = Timeline()
         for message in self.messages:
             try:
@@ -69,6 +81,14 @@ class Timeline:
 
     # Fixes encoding errors
     def to_text(self,file):
+        """
+        Converts the messages in a timeline to raw newline separated text
+        Args:
+            file: The filename to save the text to.
+
+        Returns:
+
+        """
         f = open(file,'w')
         for message in timeline.messages:
             try:
@@ -79,6 +99,11 @@ class Timeline:
 
     # Most Reacted Messages
     def message_reacs(self):
+        """
+        Returns a sorted list of messages by how many reactions they received.
+        Returns: A sorted list, from most to least reacted.
+
+        """
         mr_count = []
         for message in self.messages:
             try:
@@ -90,6 +115,19 @@ class Timeline:
 
     # Constructs a dictionary of edge frequencies where edges are (sender,tagged)
     def tag_matrix(self,tagger = True, tagged = True):
+        """
+        Constructs a dictionary of edge frequencies where edges are (sender,tagged)
+
+        Args:
+            tagger: A boolean value that indicates whether or not to distinguish between
+                    different taggers (e.g. whether or not to sum over the sender index)
+            tagged: A boolean value that indicates whether or not to distinguish between
+                    different tag recipients (e.g. whether or not to sum over the tagged index)
+
+        Returns:
+            freq: A sorted list of tuples each of the from ((sender, tagged), count)
+
+        """
         t_dict = {}
         tags = []
         for message in self.messages:
@@ -121,6 +159,21 @@ class Timeline:
 
     # Constructs a dictionary of reaction frequencies where edges (reaction wheighted) are (sender,reaction,receiver)
     def reac_matrix(self,num = None,sender = True, reaction = True, receiver = True):
+        """
+        Searches through messages and constructs a dictionary of reaction frequencies where edges (reaction weighted)
+        are tuples of the form ((sender,reaction,receiver),count)
+
+        Args:
+            num: Decides whether to slice the return list to the first num values
+            sender: Boolean that decides whether to sum over sender index in tuple
+            reaction: Boolean that decides whether to sum over reaction index in tuple
+            receiver: Boolean that decides whether to sum over receiver index in tuple
+
+        Returns:
+            freq: A dictionary of reaction frequencies where edges (reaction weighted)
+        are tuples of the form ((sender,reaction,receiver),count)
+
+        """
         reactions = []
         for message in self.messages:
             try:
@@ -154,6 +207,16 @@ class Timeline:
 
     # Creates a list of time gaps between a person's message to the group and the first reply back
     def reply_gap(self,person):
+        """
+        Creates a list of time gaps between a person's messages to the group and the first reply back
+
+        Args:
+            person: A string argument giving the name of the particpant
+
+        Returns:
+            reply_gap: A list of time differences (in seconds)
+
+        """
         reply_gap = []
         for i in range(len(self.messages)-1):
             if self.messages[i]['sender_name'] == person and self.messages[i+1]['sender_name'] != person:
@@ -161,12 +224,24 @@ class Timeline:
         return reply_gap
 
     def get_timestamps(self):
+        """
+        Retrieves the timestamps from a timeline
+
+        Returns:
+            timestamps: A list of timestamps (utc format)
+
+        """
         timestamps = []
         for message in self.messages:
             timestamps.append(message['timestamp_ms'] / 1000)
         return timestamps
 
     def raw_text(self):
+        """
+        Return the raw text (without saving or fixing formatting)
+        Returns:
+            text: Raw string of timeline text
+        """
         text = ''
         for message in self.messages:
             try:
@@ -176,8 +251,19 @@ class Timeline:
                 continue
         return text
 
+
 # Combine Messages in seperate timelines
 def combine(timelines):
+    """
+    Combines messages from a list of timelines into one larger timeline object.
+
+    Args:
+        timelines: A list of timelines to combine
+
+    Returns:
+        timeline: A new timeline that maintains timestamp ordering
+
+    """
     new_timeline = {}
     new_messages = []
     for timeline in timelines:
@@ -188,8 +274,21 @@ def combine(timelines):
     new_timeline['messages'] = list(new_timeline['messages'])
     return Timeline(data = new_timeline)
 
+
 # Will return the top 'num' words as a list of tuples [(word,num)]
 def common_words(text,pl = False):
+    """
+    Returns a sorted list of the most frequent words from a text collection.
+
+    Args:
+        text: The text to analyze
+        pl: A boolean that decides whether or not to create
+            a word cloud of the results.
+
+    Returns:
+        freq: A sorted list of tuples of the form [(word,num)]
+
+    """
     #From NTLK library
     stop_words = set(stopwords.words("english"))
 
@@ -214,8 +313,20 @@ def common_words(text,pl = False):
 
     return freq
 
+
 # Constructs a frequnecy list based off of tag_matrix
 def tag_freq(freq_tuples,num,pl = -1):
+    """
+    Constructs a frequency list based off of tag_matrix
+    Args:
+        freq_tuples: The frequency tuple list (with all indices present)
+        num: Where to slice the list
+        pl: Plot the results (no return)
+
+    Todo:
+        Fix the return values and/or merge with tag_matrix
+
+    """
 
     freq = []
     for (tagger,tagged),count in freq_tuples:
@@ -261,6 +372,25 @@ def reac_freq(reac_tuples,num,pl = -1):
 # time_difference (hours) will account for timezone differences
 # aka time_difference = 1 assumes you're one hour ahead
 def activity_plot(timestamps,time_difference = 0,daily = True):
+    """
+    Plots the number of messages sent in each hour slot during the day
+    time_difference (hours) will account for timezone differences
+    aka time_difference = 1 assumes you're one hour ahead
+
+    Args:
+        timestamps: List of utc style timestamps (seconds)
+        time_difference: Compensate for time-zone directly
+        daily: Decide to make daily or overall activity plot
+
+    Returns:
+        times: Returns timestamps converted to either hours or in the case of
+        daily = False returns timestamps unaltered
+
+    Todo:
+        It'd be nice to be able to choose a periodicity and have the timestamps
+        converted accordingly...
+
+    """
     times = []
     if daily == True:
         for stamp in timestamps:
@@ -274,6 +404,19 @@ def activity_plot(timestamps,time_difference = 0,daily = True):
 # Groups Timestamps into Clusters
 # (5-10) ~ are good numbers for min_cluster
 def cluster(timestamps,min_cluster = 7,plot = False):
+    """
+    Uses a clustering algorithm from HDBSCAN to cluster timestamps into conversation epochs.
+
+    Args:
+        timestamps: List of timestamps (utc seconds)
+        min_cluster: Minimum cluster size to consider (5-10 work well)
+        plot: Decide whether or not to plot results
+
+    Returns:
+        labels: A list giving a label to each element of timestamp. If label[i] = -1 it's classified as noise
+        and if label[i] = label[j] then then timestamp[i] and timestamp[j] are in the same cluster.
+
+    """
 
     X = np.array(timestamps)
     X = X.reshape(-1,1)
