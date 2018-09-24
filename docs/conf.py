4fd0466ffa0d5af108c950661edca94c8a06e9c7
zrobertson <zachary_w_robertson@hotmail.com>
2018-09-24 11:57:42 -0500

    Publishing Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -19,19 +19,19 @@ from mock import Mock as MagicMock
 sys.path.insert(0, os.path.abspath('../'))
 
 
-class Mock(MagicMock):
+'''class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
 
 
 MOCK_MODULES = ['numpy','matplotlib.pyplot','ftfy','json','nltk','nltk.corpus','nltk.tokenize','hdbscan','datetime','collections','wordcloud']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)'''
 
 #MOCK_MODULES = ['numpy','matplotlib.pyplot','ftfy','json','nltk','nltk.corpus','nltk.tokenize','hdbscan','datetime','collections','wordcloud']
 #for mod_name in MOCK_MODULES:
 #    sys.modules[mod_name] = mock.Mock()
-#autodoc_mock_imports = ['numpy','matplotlib.pyplot','ftfy','json','nltk','nltk.corpus','nltk.tokenize','hdbscan','datetime','collections','wordcloud']
+autodoc_mock_imports = ['numpy','matplotlib.pyplot','ftfy','json','nltk','nltk.corpus','nltk.tokenize','hdbscan','datetime','collections','wordcloud']
 
 
 # -- Project information -----------------------------------------------------
